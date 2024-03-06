3a967949548be789917e926911bccb1774b8c140
VukZh <vukfx@inbox.ru>
2024-03-06 23:10:44 +0500

    feat: create empty modules, controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ArtistController } from './artist/artist.controller';
+import { ArtistModule } from './artist/artist.module';
+import { TrackModule } from './track/track.module';
+import { AlbumController } from './album/album.controller';
+import { AlbumModule } from './album/album.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavoritesModule],
+  controllers: [AppController, ArtistController, AlbumController],
   providers: [AppService],
 })
 export class AppModule {}
